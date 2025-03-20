40a1408320ee1b9064f424e5d1f5914e0a376bcc
b4xabhishek <b4xabhishek@gmail.com>
2025-03-20 18:30:38 +0530

    remove cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,12 @@
 import cors from 'cors';
 
 const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'https://frontend-domain.com',
-    'https://matrimoney-backend.vercel.app',
-    'https://cdnjs.cloudflare.com',
-    'https://chaturvarnam.vercel.app',
-    'https://charming-hoover.65-254-80-213.plesk.page'
-  ], 
+  origin: (origin: any, callback: (arg0: null, arg1: boolean) => void) => {
+    callback(null, true); // Allow any origin dynamically
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
 };
 
-export default cors(corsOptions); 
+export default cors(corsOptions);
