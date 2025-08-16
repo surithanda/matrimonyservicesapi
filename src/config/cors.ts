ecc735bc148327d0c1eb38ca9cdb86e4797e2fd5
surithanda <suri_thanda@hotmail.com>
2025-08-16 07:39:09 -0700

    Update cors.ts

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,11 +1,28 @@
 import cors from 'cors';
 
+//const corsOptions = {
+//  origin:const allowedOrigins = ['http://localhost:3000',
+  //(origin: any, callback: (arg0: null, arg1: boolean) => void) => {
+  //  callback(null, true); // Allow any origin dynamically
+  //},
+ // credentials: true,
+ // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+ // allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+//};
+
+
+const allowedOrigins = ['http://localhost:3000', 'https://your-production-site.com'];
+
 const corsOptions = {
-  origin: (origin: any, callback: (arg0: null, arg1: boolean) => void) => {
-    callback(null, true); // Allow any origin dynamically
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block request
+    }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
 };
 
