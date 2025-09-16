8745bbd6618b66fc4811594c2a39ebc696ffc2db
surithanda <suri_thanda@hotmail.com>
2025-09-15 22:22:04 -0700

    Update cors.ts (#33)

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -11,7 +11,7 @@ import cors from 'cors';
 //};
 
 
-const allowedOrigins = ['http://localhost:3000', 'https://matrimonyservices-fm8tfyoyn-superreaders-projects.vercel.app'];
+const allowedOrigins = ['http://localhost:3000', 'https://matrimonyservices-8isqmlakx-superreaders-projects.vercel.app/'];
 
 const corsOptions = {
 
