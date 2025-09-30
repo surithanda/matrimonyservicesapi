2aef36398462cc30981c0602ddc5129e97a61ebc
Amol Pandhare <amol.rpandhare@gmail.com>
2025-09-30 10:48:03 +0530

    photo upload render possibe fix

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve uploads directory statically
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static('/photos'));
 
 // Request logging middleware
 app.use((req, res, next) => {
